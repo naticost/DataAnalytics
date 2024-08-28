866fceb47107e4e5a9593928ddb262ed18dfda6e
NatieleCosta <natisouza806@gmail.com>
2024-08-27 23:00:38 -0300

    alterando nome da tabela por alter table

--- BancodeDados/conexao.py
+++ BancodeDados/conexao.py
@@ -3,7 +3,9 @@ import sqlite3
 conexao = sqlite3.connect('banco')
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE usuarios(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100));')
+#cursor.execute('CREATE TABLE usuarios(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100));')
+
+cursor.execute('ALTER TABLE usuarios RENAME TO usuario')
 
 conexao.commit()
 conexao.close
