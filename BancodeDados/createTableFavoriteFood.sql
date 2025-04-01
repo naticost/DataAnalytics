88f3b3d5d1eddd8e8b496209231a87fbb7c0b9f1
Natiele <natisouza806@gmail.com>
2025-04-01 15:43:38 -0400

    criando tabela de food

---
+++ BancodeDados/createTableFavoriteFood.sql
@@ -0,0 +1,7 @@
+create table favorite_food(
+	person_id smallint unsigned,
+    food varchar(20),
+    constraint pk_favorite_food primary key (person_id, food),
+    constraint fk_favorite_food_person_id foreign key(person_id) references person(person_id)
+);
+
